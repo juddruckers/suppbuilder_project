8115d767383a9dc8d39183b80324e852a622e86e
Judd Arias <juddarias@gmail.com>
2017-02-04 15:09:13 -0800

    Update settings.py

--- suppbuilder_project/settings/settings.py
+++ suppbuilder_project/settings/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'w@t$(&@2=s$sc0pl)dt=_d6qv&=w5c397^$ib^%a+be$htps5$'
 # SECRET_KEY = os.environ.get('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
