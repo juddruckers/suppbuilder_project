b654ed81c54508e69f50e9f7b80bbfcd9e9b0b42
Judd <juddarias@gmail.com>
2017-11-03 11:11:21 -0700

    added mission statement section with minor styling

--- suppbuilder/static/suppbuilder/js/script.js
+++ suppbuilder/static/suppbuilder/js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
 
-  // icon animation on scroll
-
+  /*
+  make the how it works section slide up when the 
+  user scrolls down to that section.
+  offset by 70%, the default would have the entire
+  page blank before the section slides up
+  */
   $(".js--wp-1").waypoint(function(direction){
     $(".js--wp-1").addClass('animated slideInUp');
   }, {
     offset: "70%"
   });
 
+
+    $(".js--wp-2").waypoint(function(direction){
+    $(".js--wp-2").addClass('animated fadeIn');
+  }, {
+    offset: "70%"
+  });
 });
